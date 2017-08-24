ef290da6e22e09a308be031408cd614fda473f24
Luis Pallares <thepanchi@gmail.com>
2017-08-24 09:16:57 +0200

    config files

---
+++ config/enviroment_variables.sample.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+export ES_GTD_FILES=/Users/panchicore/www/es-gtd/data/sample.csv
+export ES_GTD_FILES=/Users/panchicore/www/es-gtd/data/*.csv
+export ES_GTD_HOST=http://localhost:9200/
+export ES_GTD_USER=elastic
+export ES_GTD_PASSWORD=changeme
+export ES_GTD_INDEX=gtd
+export SLACK_GTD_NOTIFICATIONS_ENABLED=1
+export SLACK_GTD_NOTIFICATIONS_URL=https://hooks.slack.com/services/XXXX/YYYYY/ZZZZZ
